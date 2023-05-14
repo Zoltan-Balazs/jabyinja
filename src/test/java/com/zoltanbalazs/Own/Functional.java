ec166aebd3ca21d96d14d3357253f107dc6f176d
Zoltán Balázs <git@zoltanbalazs.com>
2023-05-14 23:22:09 +0200

    Changed Functional test

--- src/test/java/com/zoltanbalazs/Own/Functional.java
+++ src/test/java/com/zoltanbalazs/Own/Functional.java
@@ -3,19 +3,28 @@ package com.zoltanbalazs.Own;
 import java.util.function.Function;
 
 class Executor<T> {
+    Function<T, T> endomorphism;
+    T target;
+
     public Executor(Function<T, T> endomorphism, T target) {
-        System.out.println(endomorphism.apply(target));
+        this.endomorphism = endomorphism;
+        this.target = target;
     }
 
     public Executor(T target, Function<T, T> endomorphism) {
+        this.endomorphism = endomorphism;
+        this.target = target;
+    }
+
+    public void execute() {
         System.out.println(endomorphism.apply(target));
     }
 }
 
 public class Functional {
     public static void main(String[] args) {
-        Executor<Integer> ex1 = new Executor<Integer>(a -> a + 1, 10);
-        // var ex2 = new Executor<Integer>(10, a -> a + 2);
+        new Executor<Integer>(a -> a + 1, 10).execute();
+        // new Executor<Integer>(10, a -> a + 2);
         // var ex3 = new Executor<Function<Integer, Integer>>(f -> a -> f.apply(a),
         // (Integer a) -> a);
         // var ex4 = new Executor<Function<Integer, Integer>>((Integer a) -> a, f -> a
