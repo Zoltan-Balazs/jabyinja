426fbdf593d54292abac144ced2b7658373229a0
Zoltán Balázs <git@zoltanbalazs.com>
2023-05-27 01:10:35 +0200

    Fixed unused variable warning

--- src/test/java/com/zoltanbalazs/PTI/_02/_02/Circle.java
+++ src/test/java/com/zoltanbalazs/PTI/_02/_02/Circle.java
@@ -1,11 +1,9 @@
 package com.zoltanbalazs.PTI._02._02;
 
 public class Circle {
-    private double x, y, radius;
+    private double radius;
 
     Circle(double x, double y, double radius) {
-        this.x = x;
-        this.y = y;
         if (radius < 0) {
             throw new IllegalArgumentException("Non-positive radius!");
         } else {
@@ -26,11 +24,9 @@ public class Circle {
     }
 
     public void setX(double x) {
-        this.x = x;
     }
 
     public void setY(double y) {
-        this.y = y;
     }
 
     public double getArea() {
